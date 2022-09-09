b468720b857a9ffb9d235d894ba603e7a6f3a83b
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-09 16:18:10 -0700

    Alx Low Level Programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /* more headers goes there */
 
-
+/* main -print by using if condition */
 
 /* betty style doc for function main goes there */
 
@@ -13,6 +13,7 @@ int main(void)
 
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
