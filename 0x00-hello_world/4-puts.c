f2faf453175c21878086acc85971a682beb65a13
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-08 18:16:19 -0700

    Alx Low Level Programming _ C Language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+/*int main(void) :  Main function*/
 int main(void)
-/*main funciton*/
 
-{
-
+		{
+	/*puts() print this Programming is like building a multilingual puzzle*/
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	/*puts() function print below text*/
+/*this programm return 0*/	
 	return (0);
-	/*main function return 0*/
+	
 		}
