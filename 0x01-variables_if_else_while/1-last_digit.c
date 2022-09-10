97d51d11c0380b154effadbe85b3ceb8f10d1062
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 10:14:24 -0700

    Alx Low Level Programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,10 @@
-#include <stdio.h>                                                                                                                                   
-
-#include <stdlib.h>                                                                                                                                  
-
-                                                                                                                                                     
-
-#include <time.h>                                                                                                                                    
-
-/* more headers goes there */                                                                                                                        
-
-                                                                                                                                                     
-
-                                                                                                                                                     
-
-                                                                                                                                                     
-
-/* betty style doc for function main goes there */                                                                                                   
-
-                                                                                                                                                     
-
-/**                                                                                                                                                  
+#include <stdio.h>                                                              
+#include <stdlib.h>                                                            
+#include <time.h>                                                               
+/**
+ * main - print with condition
  *
- * main - print last digit of a number depending conditions                                                                                   
- *                                                                                                                                                   
- *
- * Return: Always 0 (Success)                                                                                                              
+ * Return: 0
  *
  */
 
@@ -41,12 +22,10 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_Digit);                           
 	if (last_Digit == 0)                                                                    
 	       	printf("Last Digit of %d is %d and 0\n", n, last_Digit);                                                                  
-	if (last_Digit < 6 && last_Digit != 0)                                                                                              
-	       	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_Digit);                                                       
+	if (last_Digit < 6 && last_Digit != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_Digit);                                                       
                                                                  
-	return (0);                                                                                                                                  
-
-									                                                                          
+	return (0);                                                            
 }                                                                
 
 
