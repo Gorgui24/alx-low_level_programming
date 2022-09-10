2ef967bbe4c8727ba847b6f471a19f8ee5dc5f61
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 10:02:59 -0700

    Alx Low Level Programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -36,13 +36,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;                                                                                        
 	/* your code goes there */                                                             
 
-	int last_Digit = n % 10;                                                                             
+	int last_Digit = 
+		last_Digit = n % 10;                                                                             
 
 	if (last_Digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_Digit);                           
 	if (last_Digit == 0)                                                                    
 	       	printf("Last Digit of %d is %d and 0\n", n, last_Digit);                                                                  
-	if (last_Digit < 6 && != 0)                                                                                              
+	if (last_Digit < 6 && last_Digit != 0)                                                                                              
 	       	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_Digit);                                                       
                                                                  
 	return (0);                                                                                                                                  
