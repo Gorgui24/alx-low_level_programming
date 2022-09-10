20b8af2a78b7cc6a99cd2bb090b9509d369be1b9
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 06:27:50 -0700

    Alx Low Programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numeric
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\t,", n);
+	}
+	
+	return (0);
+}
+
