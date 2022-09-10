081ce52d56a6a9064a4578831945366b6988b740
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 03:37:13 -0700

    Alx low level programing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase except q and e
+ * 
+ * Return: 0
+ *
+ *
+ */
+
+int main(void)
+{
+	char car;
+	for (car = 'a'; car < 'e'; car++)
+		putchar(car);
+	for (car = 'f'; car < 'q'; car++)
+		putchar(car);
+	for (car = 'r'; car <= 'z'; car++)
+		putchar(car);
+	print('\n');
+
+}
+
