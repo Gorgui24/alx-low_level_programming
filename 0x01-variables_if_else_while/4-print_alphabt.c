dbefed5e1d8b139df7f6cb01126b5c378d5bcf75
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 11:39:41 -0700

    Alx Low Level Programming

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,11 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z';)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch == 'e' || ch == 'q')
 			continue;
 			putchar(ch);
-		ch++;
 	}
 
 	putchar('\n');
