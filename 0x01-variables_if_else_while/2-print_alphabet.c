70670f6f835d2d4f29447bf8366cdefa4c6ef941
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-09 18:13:21 -0700

    Alx Low Programming Software

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-/** 
+#include <stdio.h>
+
+/**
  * main - print letters a to z
  *
  * Programm return to 0
  */
 int main(void) 
 {
-
 	char l = 'a';
 
-	for (l = 'a'; a <= 'z'; z++)
+	for (l = 'a'; l <= 'z'; l++)
 		printf(l);
 	return (0);
 
