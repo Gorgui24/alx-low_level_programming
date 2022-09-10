0fa646edcb84324d19c8a962373f7c7fa01d7c56
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 06:13:57 -0700

    Alx Low Programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print a to z in uppercase and lowercase
+ *
+ * Description : using the main function
+ * This programm print a to z
+ * Return  : 0
+ */
+int main(void) 
+{
+	char ch;
+	int num = 0;
+	
+	for (num = 0; num < '10'; ++num)
+		putchar(num);
+	for (ch = 'a'; ch <= 'f'; ++ch)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
+
