4fb4cb023f30eaa09d331a05e912e9cb4d0b0ac6
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 05:58:12 -0700

    Alx Low Programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numeric
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; n++)
+		putchar(num);
+	putchar('\n');
+	
+	return (0);
+}
+
