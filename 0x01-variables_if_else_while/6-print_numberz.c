c0306101d68e0cb4f04a5f7ddb0741ffdfe4d722
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 12:09:53 -0700

    Alx Low Level Programmingclear

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int num;
+	char num;
 	
-	for (num = 0; num < 10; num++)	
+	for (num = '0'; num <= '9'; num++)	
 	{
-		putchar((char)num);
+		putchar();
 	}
 	putchar('\n');
 	return (0);
