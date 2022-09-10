0e3897043a4e416e2e4eb2a088de0d35d1857ccd
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-09 17:56:44 -0700

    Alx Low Programming Software

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/** 
+ * main - print letters a to z
+ *
+ * Programm return to 0
+ */
+int main(void) 
+{
+
+	char l;
+
+	for (i = 'a'; a <= 'z'; z++)
+		printf(l);
+	return (0);
+
+
+
+}
