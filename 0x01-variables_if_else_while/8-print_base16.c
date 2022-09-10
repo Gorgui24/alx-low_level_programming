1caa6e78f1c468a86b7cf046e1cd5345ee0b7b35
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-10 07:08:03 -0700

    Alx Low Programming

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,10 @@ int main(void)
 {
 	char ch;
 	int num = 0;
-	
+	char chn = num + '0';
+
 	for (num = 0; num < 10; ++num)
-		putchar(num);
+		putchar(chn);
 	for (ch = 'a'; ch <= 'f'; ++ch)
 		putchar(ch);
 	putchar('\n');
