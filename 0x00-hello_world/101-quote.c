2ef0aaa4d4a49d455b5187a1b6bc12ef01393a6b
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-11 08:39:12 -0700

    Alx Low Level Programing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *     main - print string in console
+ *
+ *     puts function print on screen
+ *
+ *     Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+		printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (0);
+}
+
+
