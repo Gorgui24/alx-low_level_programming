3f54c03300b1f9a79c661ceaac8123f1ae15209f
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-11 08:09:03 -0700

    Alx Low Level Programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *     main - print string in console
+ *
+ *     puts function print on screen
+ *
+ *     Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
+
+
