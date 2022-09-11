cad27c684085406e13089bd0ec2cd33750b9eaf5
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-11 03:41:54 -0700

    Alx Low Level Programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,18 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 1; n <= 9; n++)
 	{
 		for (m = 0; m <= 8; m++)
 		{
 			putchar(m + '0');
+		}
 			putchar(n + '0');
 			putchar(',');
 			putchar(' ');
-		}
+		
 
 	}
 	return (0);
 }
-
+i
