4536fec4bc5864369091f6387beaba25d83fc0b5
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-12 12:40:03 -0700

    Alx Low Level Programming

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print numeric
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n, m, p;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 1; m <= 9; m++)
+		{
+			if (m > n)
+			{
+				for (p = 2; p <= 9; p++)
+				{
+					if (p > m)
+					{
+						putchar(n + '0');
+						putchar(m + '0');
+						putchar(p + '0');:
+						if (n != 8)
+						{
+						putchar(',');
+						putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
