3c4517909afab6a1d4d3290b66a07381602c7ed4
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-13 12:10:19 -0700

    Alx Low Level Programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - print _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+
+}
+
+void print_alphabet(void)
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+       putchar(ch +'0');	
+}
