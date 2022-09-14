1d248781144756a2d109dd70802a1eb85ca7dfb5
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-14 07:24:33 -0700

    Alx Low Level Programming

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * add - print sum of two  numbers
- * @a @b: number to sum
- * Return: 0
+ * @a: first number to add
+ * @b: second number to add
+ * Return: resturn sum of thes two numbers
  */
 int add(int a, int b)
 {
 	int sum = a + b;
+	
 	return (sum);
 }
