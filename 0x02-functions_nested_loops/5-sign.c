4e8d5eef9a479c67b75bdfe584ca9d0a3c8201c5
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-13 19:10:10 -0700

    Alx Software Programming

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,13 +11,13 @@ int print_sign(int n)
 		return (1);
 		_putchar('+');
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	
 	} 
-	else if (n < 0)
+	if (n < 0)
 	{
 		return (-1);
 		_putchar('-');
