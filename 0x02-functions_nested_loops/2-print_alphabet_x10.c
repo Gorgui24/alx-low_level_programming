a0fe03110dfa65b05e6d3e8c1e69ba58dfbf89f8
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-13 17:44:55 -0700

    Alx Low Level Programming

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - function to print x10 a to z
+ * print_alphabet_x10 - function to print x10 (a to z)
  *
  */
 void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
