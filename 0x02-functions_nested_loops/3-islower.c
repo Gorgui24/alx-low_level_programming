6e48ecfb628fa5c8c60ba0552c123eb46f352bd7
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-13 18:05:29 -0700

    Alx Low Level Programming

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print x10 (a to z)
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		_putchar(c + '0');
+		return (1);
+	}
+	else
+	{
+		_putchar(c + '0');
+		return (1);
+	}
+	_putchar('\n');
+	
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,6 +8,7 @@ int main(void)
 {
 	print_alphabet();
 	print_alphabet_x10();
+	int _islower(int c);
 	return (0);
 
 }
