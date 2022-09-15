0c90b9d9dc4039a917f7594b61c5309b2011546d
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-15 14:37:07 -0700

    Alx Low Level Programming

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_square - print square #
+ * @size: parmater that define size square of '#' to print
+ */
+void print_square(int size)
+{
+	int count = 1;
+
+	if (size >= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (size >= count)
+		{
+			int i = 1;
+
+			while (n >= i)
+			{
+				putchar('#');
+				i++;
+			}
+			count++;
+			putchar('\n');
+		}
+	}
+
+}
