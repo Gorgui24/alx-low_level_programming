c1379e72362ccd9af8755bfdcb50652f51bf03e3
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-15 06:52:16 -0700

    Alx Low Level Programming

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,11 @@
  * mul - print multi equal to a * b
  * @a: first value
  * @b: second value 
- * Return: multi, a * b
+ * Return: return multiplication of two numbers
  */
 int mul(int a, int b)
 {
-	int multi;
+	int multi = a * b;
 
-	multi = a * b;
 	return (multi);
 }
