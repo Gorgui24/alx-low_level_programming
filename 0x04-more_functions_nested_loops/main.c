07156060a563486ffbe062e73c7d4679d7c98821
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-15 06:44:30 -0700

    Alx Low Level Programming

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -18,6 +18,9 @@ int main(void)
 	 printf("%c: %d\n", c, _isdigit(c));
 	 c = 'a';
 	 printf("%c: %d\n", c, _isdigit(c));
+
+	 printf("%d\n", mul(98, 1024));
+	 printf("%d\n", mul(-402, 4096));
 	_putchar('\n');
 	return (0);
 
