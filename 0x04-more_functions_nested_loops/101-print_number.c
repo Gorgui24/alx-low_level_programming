6498d4aca9e613d905280bb856b6a66fd5c2c37a
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-15 15:40:50 -0700

    Alx Low Level Programming

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_number - print number
+ * @n: parameter to print number
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (n > 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
+}
