74a74e6db9c77cf69ca0bf1d969c0d3496c9c3f4
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-15 11:36:29 -0700

    Alx Low Level Programming

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - print line like _
+ */
+void print_line(int n)
+{
+	int  count = 0;
+
+	while (n <= count)
+	{
+		_putchar('_');
+		count++;
+	}
+	_putchar('\n');
+}
