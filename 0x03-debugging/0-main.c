b7d6822710da6f25cc94538a5226be04be9c79f1
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-15 18:14:51 -0700

    Alx Low Level Programming*

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main - check the code.
- *
- * Retun: Alwaays 0
+ * main - tests function that prints if integer is positive or negative
+ * Retun: 0
  */
 int main(void)
 {
