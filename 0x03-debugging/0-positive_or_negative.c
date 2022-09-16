66478bcc63a632e7e7da61b78382b50f34e2262f
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-15 19:50:50 -0700

    Alx Low Level Programming

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - print a number depending to if condition
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	/* your code goes there */
+	return (0);
+
+}
