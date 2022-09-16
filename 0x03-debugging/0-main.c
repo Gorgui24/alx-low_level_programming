f50966601a8b30fc1d10ee431f0bc1967ed98f4e
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-15 18:08:41 -0700

    Alx Low Level Programming*

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Retun: Alwaays 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
