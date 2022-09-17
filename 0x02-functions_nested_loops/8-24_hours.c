fe24a1df79c1e92ac3efb29d511fa8c3ce9c3730
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-17 13:13:10 -0700

    Alx Low Level Programming

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,21 +5,29 @@
  */
 void jack_bauer(void)
 {
-	int h, hs;
+	int h, hs, mn, mns;
 
 	for (h = 0; h <= 2; h++)
 	{
 		for (hs = 0; hs <= 9; hs++)
 		{
-			if (h <= hs)
+			for (mn = 0; mn <= 5; mn++)
 			{
-				if (hs <= 3 || h < 2)
+				for (mns = 0; mns <= 9; mns++)
 				{
-				_putchar(h + '0');
-				_putchar(hs + '0');
+					if (h < 2 || hs <= 3)
+					{
+						putchar(h + '0');
+						putchar(hs + '0');
+						putchar(':');
+						putchar(mn + '0');
+						putchar(mns + '0');
+						putchar('\n');
+					}
 				}
 			}
-			_putchar('\n');
 		}
 	}
 }
+
+
