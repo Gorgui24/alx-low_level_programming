451eeb5b4c54ed49466ddff57b0ac6ccc9cbb07c
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-18 06:18:54 -0700

    Alx Low Level Programming

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,11 +5,11 @@
  */
 void print_times_table(int n)
 {
-	int cl, li, mul, fd, ld, fds;
+	int cl, li, mul, fd, ld;
 
-	for (cl = 0; cl <= n; cl++)
+	for (cl = 0; cl <= 9; cl++)
 	{
-		for (li = 0; li <= n; li++)
+		for (li = 0; li <= 9; li++)
 		{
 			mul = cl * li;
 			if (li > 0)
@@ -20,16 +20,12 @@ void print_times_table(int n)
 					_putchar(' ');
 				_putchar(mul + '0');
 			}
-			if (mul >= 10 && mul < 99)
+			if (mul >= 10)
 			{
 				fd = mul / 10;
 				ld = mul % 10;
 				_putchar(fd + '0');
 				_putchar(ld + '0');
-			}
-			if (mul >= 100)
-			{
-				fd = mul / 10;
 			}
 			if (li != 9)
 				_putchar(',');
