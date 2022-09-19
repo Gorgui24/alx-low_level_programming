bd01aa656ab824fa0930a763fa3c6394855dcda3
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-19 06:29:57 -0700

    Alx Low Level Programming

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <string.h>
-
 /**
- * _strlen - function to get size of char 
+ * _strlen - define size of string
  * @s: parameter to get size
- *
+ * Return: int
  */
 int _strlen(char *s)
 {
