ec21117e23a981d181cc4e1e3bf36c78ac7693f1
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-19 13:58:53 -0700

    Alx Low Programming Software

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - print string
+ *@str: string to print
+ */
+void puts_half(char *str)
+{
+	int n;
+	int j = (strlen(str) - 1) / 2;
+	int k = strlen(str);
+
+	for (n = j; n < k; n++)
+		_putchar(str[n]);
+	_putchar('\n');
+}
