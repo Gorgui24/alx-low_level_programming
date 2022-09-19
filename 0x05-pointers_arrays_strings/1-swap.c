171cfe29d2eeb2a9e42dce03986e00d9d36c99e9
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-18 22:19:11 -0700

    Alx Low Level Programming

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two numbers
+ * @a: first number to swap with second number(b)
+ * @b: second number to swap with first(a)
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
