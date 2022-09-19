5556f279acdfefc53cae51b37785494000c24d3a
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-19 15:56:34 -0700

    Alx Low Level Programming

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - print number
+ *@a: table string to print
+ *@n: index of table
+ */
+void print_array(int *a, int n)
+{
+	for (n = 0; n < strlen(s); n++)
+	{
+		printf(str[n]);
+		if (n != (strlen(s) - 1))
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
