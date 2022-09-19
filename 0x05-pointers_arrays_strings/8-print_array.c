7349f304b45dffb852081ce4c31b6a1e49667d4d
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-19 16:59:31 -0700

    Alx Low Level Programming

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,7 +8,7 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	long i;
 
 	for (i = 0; i < n; i++)
 	{
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcpy - print string
+ *@dest: string to receive copy
+ *@src: string to send copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
