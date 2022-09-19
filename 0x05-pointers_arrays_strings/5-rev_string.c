0939dcb983697e626e3a7f3f9e0027115ef6bd58
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-19 10:32:43 -0700

    Alx Low Level Programming

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * prev_string - print string
+ * @s: string to print
+ * Return: int
+ */
+void rev_string(char *s)
+{
+	int i, j = strlen(s) - 1;
+
+	for (i = j; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,5 @@ void _putchar(char);
 void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
-
+void rev_string(char *s);
 #endif
