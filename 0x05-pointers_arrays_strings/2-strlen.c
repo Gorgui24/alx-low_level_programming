f3639833023815b033aca1c0f0e3df0734ce531d
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-19 06:19:17 -0700

    Alx Low Level Programming

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - function to get size of char 
+ * @s: parameter to get size
+ *
+ */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,6 @@
 
 void _putchar(char);
 void reset_to_98(int *n);
-
+int _strlen(char *s);
 
 #endif
