f754d6aae9610a2da8668f9130e7a894ea8b53c3
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-20 15:11:44 -0700

    Alx Low Level Programming

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_atoi - convert char to int
+ *@s: pointer string
+ *Return: pointerint
+ */
+int _atoi(char *s)
+{
+	int i, j =  strlen(s);
+	int *t;
+
+	for (i = 0; i < j; i++)
+	{
+		t[i] = int(s[i]);
+	}
+	return (t);
+}
