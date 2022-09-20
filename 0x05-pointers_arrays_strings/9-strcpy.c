10c6852f8205b20838b68aff22518a471db0d521
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-20 12:21:39 -0700

    Alx Low Level Programming

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - print string
+ *@str: string to print
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
