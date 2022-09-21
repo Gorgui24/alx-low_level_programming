eb1df51ce0156404c49a2a9ed4503fbc272497a0
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-21 12:54:12 -0700

    Alx Low Level Programming

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - copy string
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: pointer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strncmp(s1, s2));
+}
