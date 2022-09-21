4a4c24fce2bac35d1e18668546130fd22dee1b4c
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-21 12:41:17 -0700

    Alx Low Level Programming

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * _strncat - copy string
  * @dest: parameter to copy string
  * @src: parameter to send string
- * n: string to copy
+ * @n: string to copy
  * Return: pointer
  */
 char *_strncat(char *dest, char *src, int n)
