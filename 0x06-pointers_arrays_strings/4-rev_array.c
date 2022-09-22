6a14cf4263cab94977a50d80591ee67ded2d8d78
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-22 16:03:39 -0700

    Alx Low Level Programming

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,9 +15,45 @@ void reverse_array(int *a, int n)
 	while (i >= 0)
 	{
 		if (j != i)
-			printf(", ");
-		printf("%d", a[i]);
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		if (a[i] <= 9)
+		{
+			_putchar(a[i] + 'i');
+		}
+		else if(a[i] <= 99)
+		{
+			int k = a[i] % 10;
+			int l = a[i] / 10;
+
+			_putchar(l +'0');
+			_putchar(k +'0');
+		}
+		else if (a[i] <= 999)
+		{
+			int k = a[i] % 10;
+			int m = a[i] % 100 / 10;
+			int l = a[i] / 100;
+
+			_putchar(l + '0');
+			_putchar(m + '0');
+			_putchar(k + '0');
+		}
+		else if(a[i] <= 9999)
+		{
+			int k = a[i] % 10;
+			int m = a[i] % 1000 / 100;
+			int n = a[i] % 1000 % 100 / 10;
+			int l = a[i] / 1000;
+
+			_putchar(l + '0');
+			_putchar(n + '0');
+			_putchar(m + '0');
+			_putchar(k + '0');
+		}
 		i--;	
 	}
-	printf("\n");
+	putchar('\n');
 }
