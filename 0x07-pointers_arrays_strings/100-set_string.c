3f9921c07d0292418722f46e8c5e229ff2d7c806
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-27 06:43:07 -0700

    Alx Low Level Programming

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include<string.h>
+/**
+ * set_string - copy pointer data to double pointer
+ * @s: pointer parameter
+ * @to: double pointer parameter
+*/
+void set_string(char **s, char *to)
+{
+	s = &to;
+}
