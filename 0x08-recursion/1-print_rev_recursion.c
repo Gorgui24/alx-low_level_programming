37be07c17f126702b3a8d7a12c83ced98b09a944
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-28 15:46:48 -0700

    Alx Low Level Programming

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _print_rev_recursion - print by recursion
+ * @s: pointer parameter
+ */
+void _print_rev_recursion(char *s)
+{
+	int  j = strlen(s) - 1;
+
+	if (j < 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[j]);
+	_puts_recursion(s - 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,5 +2,6 @@
 #define MyHeaderFile
 void _putchar(char ch);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 #endif
 
