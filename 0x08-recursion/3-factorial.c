c0bd097419fa69fd71b3d3cf39812cae2a913328
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-28 17:15:55 -0700

    Alx Low Level Programming

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _factorial - number factorila
+ * @n: integer parameter
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n > 0)
+	{
+		n = n * factorial(n - 1);
+	}
+}
