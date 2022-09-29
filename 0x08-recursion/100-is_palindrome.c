48d905aa731aa520e6146f97877f346cef66a0bb
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-29 13:05:13 -0700

    Alx Low Level Programming

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,11 +7,16 @@
  */
 int is_palindrome(char *s)
 {
-	int j = strlen(s) - 1, k = strlen(s) - 2;
+	int j = strlen(s) - 1, k = strlen(s) - 2, l = strlen(s) - 3;
 
 	if ((s[j] == s[0] && s[k] == s[1]) || *s == '\0')
 	{
 		return (1);
+	}
+	else if (j > 7)
+	{
+		if ((s[j] == s[0] && s[k] == s[1] && s[l] == s[2]) || s == '\0')
+			return (1);
 	}
 	else
 		return (0);
