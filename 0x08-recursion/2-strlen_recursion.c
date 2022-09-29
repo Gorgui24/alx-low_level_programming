9b302e315226193ffb58cc2c5520560be5675088
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-28 17:03:53 -0700

    Alx Low Level Programming

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strlen_recursion - print size of string
+ * @s: pointer parameter
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int  j = strlen(s);
+
+	return (j);
+}
