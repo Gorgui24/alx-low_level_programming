42244ca379eeb7e0bff7e4db6f758ddc94fbf093
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-30 13:03:12 -0700

    Alx Low Level Programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,19 @@
 #include<string.h>
 /**
  * main - print name of program
- * @argc: number of program
  * @argv: string by line
  * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	int i, j = strlen(argv[0]);
-	char *s = argv[0];
-
-	for (i = 0; j < j; i++)
-		_putchar(s[i]);
-	_putchar('\n');
-	return (0);
+	if (argc == 0)
+	{
+		int i, j = strlen(argv[0]);
+		char *s = argv[0];
+		
+		for (i = 0; i < j; i++)
+			_putchar(s[i]);
+		_putchar('\n');
+	}
+		return (0);
 }
