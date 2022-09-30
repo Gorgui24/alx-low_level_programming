2d1232f86270e0d10d0c682de124f7dc9f64a3f0
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-30 15:38:15 -0700

    Alx Low Level Programming

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - print name of program
+ * @argv: string by line
+ * @argc: number of line
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+
+	if (argc <= 0)
+		printf("error\n");
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
