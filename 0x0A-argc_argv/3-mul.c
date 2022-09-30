68d160a7a84844acaad13115f23e1ddac09047e5
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-30 15:40:41 -0700

    Alx Low Level Programming

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,9 +11,9 @@ int main(int argc, char *argv[])
 {
 
 	if (argc <= 0)
-		printf("error\n");
+		printf("Error\n");
 	else
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
-	return (0);
+	return (1);
 }
