6ab15ce4180e1579fb3e91b33b380aea219ef3b3
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-30 14:10:27 -0700

    Alx Low Level Programming

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - print name of program
+ * @argv: string by line
+ * @argc: number of line
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	_putchar(argc + 1);
+	return (0);
+}
