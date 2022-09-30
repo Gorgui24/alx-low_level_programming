b02cef6f2a504a2a34e4b5949f48245ad6a295f5
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-30 13:10:10 -0700

    Alx Low Level Programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,14 @@
 /**
  * main - print name of program
  * @argv: string by line
+ * @argc: number of line
  * Return: always 0
  */
 int main(int argc, char *argv[])
 {
 	int i, j = strlen(argv[0]);
 	char *s = argv[0];
-	
+
 	for (i = 0; i < j; i++)
 		_putchar(s[i]);
 	_putchar('\n');
