25022e4c4fed42808f647a48e80013d86c86bc80
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-09-30 18:36:35 -0700

    Alx Low Level Programming

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -7,29 +7,7 @@
  */
 int main(void)
 {
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	
-	int a;
-	int b;
-
-	a = 98;
-	b = 42;
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
-	
-	char s[10] = "My School";
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
-
-
-
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 
 }
