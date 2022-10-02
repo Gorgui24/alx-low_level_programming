eb9f189fbd6fea54a2147eae2bcd38f76c091990
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-02 14:16:31 -0700

    Alx Low Level Programming

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) >= '0' && atoi(argv[i]) <= '9')
+			if (atoi(argv[i]))
 			{
 				sum = sum + atoi(argv[i]);
 			}
