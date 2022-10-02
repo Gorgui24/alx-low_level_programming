447f9967067039ce07cc9aaa8160da5fff223537
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-02 14:14:54 -0700

    Alx Low Level Programming

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - print name of program
+ * @argv: string by line
+ * @argc: number of line
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc <= 0)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) >= '0' && atoi(argv[i]) <= '9')
+			{
+				sum = sum + atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
