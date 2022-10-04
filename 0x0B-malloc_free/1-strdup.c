ae82e43d2a5b3404d194b93aa78c72a1d50c4ba0
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-04 16:32:46 -0700

    Alx Low Level Programming

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,13 +8,16 @@
  */
 char *_strdup(char *str)
 {
-	int j = strlen(str) + 1;
+	int j = strlen(str) + 1, i;
+	char *ch;
 
 	if (str == NULL)
 		return (NULL);
-	else
-	{
-		str = malloc(sizeof(*str) * j);
-	}
-	return (str);
+	ch = malloc(sizeof(*str) * j);
+	if (ch == NULL)
+		return (NULL);
+	j = j - 2;
+	for (i = 0; i < j; i++)
+		ch[i] = str[i];
+	return (ch);
 }
