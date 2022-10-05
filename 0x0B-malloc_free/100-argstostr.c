000550fec3fb6a8038264d0b8f8057fe7477a63a
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-04 19:40:31 -0700

    Alx Low Level Programming

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,7 +14,7 @@ char *argstostr(int ac, char **av)
 	char *ch;
 
 	s = 0, k = 0;
-	if (ac == NULL || av == NULL)
+	if (av == NULL)
 	{
 		return (NULL);
 	}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,6 @@
 void _putchar(char ch);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
 #endif
 
