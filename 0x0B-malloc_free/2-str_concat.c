ce2d3536cde0abba3304baa46573c4f6dafdaf14
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-04 17:27:10 -0700

    Alx Low Level Programming

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+/**
+ * str_concat - print by recursion
+ * @s1: pointer parameter
+ * @s2: pointer parameter
+ * Return: parameter
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ch;
+	int i = strlen(s1), k = strlen(s2), j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	ch = malloc(sizeof(ch) * i);
+	for (j = 0; j < i; i++)
+	{
+		ch[i] = s1[i];
+	}
+	ch[i] = " ";
+	for (j = 0; j < i; i++)
+		ch[j + i + 1] = s2[j];
+	return (ch);
+}
