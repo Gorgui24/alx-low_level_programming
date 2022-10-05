67cf9448ffb69b089887b5cfbf1e89d45520be7a
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-04 17:37:48 -0700

    Alx Low Level Programming

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,14 +13,14 @@ char *str_concat(char *s1, char *s2)
 	char *ch;
 	int i = strlen(s1), k = strlen(s2), j;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 		return (NULL);
 	ch = malloc(sizeof(ch) * (i + k));
 	for (j = 0; j < i; j++)
 	{
 		ch[j] = s1[j];
 	}
-	ch[i] = '\0';
+	ch[i] = ' ';
 	for (j = 0; j < k; j++)
 		ch[j + i + 1] = s2[j];
 	return (ch);
