f51c1bf567e73d79c56ba459ac06b53cc18fa945
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-06 15:02:36 -0700

    Alx Low Level Programming

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include<string.h>
+#include<stdlib.h>
+/**
+ * calloc - return adress
+ * @nmemb: int parameter
+ * @size: int parameter
+ * Return: pointer address
+ */
+void *_calloc(unsigned int nmemb, unsigned int size);
+{
+	void *ch;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ch = malloc(sizeof(char) * (size * nmb));
+
+	if (ch == NULL)
+		return (NULL);
+	return (ch);
+}
