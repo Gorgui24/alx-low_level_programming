3871845e8d7646cb2c95c1e4a99e4d2497edb2ad
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-06 16:14:28 -0700

    Alx Low Level Programming

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include<string.h>
+#include<stdlib.h>
+/**
+ * array_range - return adress
+ * @min: int parameter
+ * @max: int parameter
+ * Return: pointer address
+ */
+int *array_range(int min, int max)
+{
+	int *ch;
+	int i = 0, n = min;
+
+	if (min > max)
+		return (NULL);
+
+	ch = malloc((max - min + 1) * sizeof(int));
+
+	if (ch == NULL)
+		return (NULL);
+	while (n <= max - min)
+	{
+		ch[i] = n;
+		n++;
+		i++;
+	}
+	return (ch);
+}
