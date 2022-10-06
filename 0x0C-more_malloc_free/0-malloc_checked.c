613fd86e90de9ae3094b6a7d89ac0f6cf78ef365
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-06 12:49:17 -0700

    Alx Low Level Programming

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<string.h>
+#include<stdlib.h>
+/**
+ * malloc_checked - return adress
+ * @b: int parameter
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ch;
+
+	if (b == 0)
+		return (NULL);
+	ch = &b;
+}
