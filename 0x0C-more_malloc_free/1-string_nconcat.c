2ccaadaeb4375f687221b70a57bd1fce1337a535
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-06 14:15:41 -0700

    Alx Low Level Programming

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	ch = malloc(sizeof(char) * (strlen(s1) + n) + 1);
+	ch = malloc(sizeof(char) * (strlen(s1) + n + 1));
 	if (ch == NULL)
 		return (NULL);
-	k = strlen(s1) - 1;
-	m = strlen(s2) -1;
+	k = strlen(s1);
+	m = strlen(s2);
 	for (i = 0; i < k; i++)
 		ch[i] = s1[i];
 	if (n > m)
