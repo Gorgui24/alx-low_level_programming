31270c30648095de1c9501984c3babaf8bf1aa50
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-09 09:25:29 -0700

    Alx Low Level Programming

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,23 +10,23 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *ch;
-	int i;
+	char *ch;
+	unsigned int i;
 
 	if (old_size == 0 && new_size == 0)
-		return (NULL)
+		return (NULL);
 	ch = malloc(old_size);
 	if (ch == NULL || old_size < new_size)
 		ch = malloc(new_size);
 	if (old_size >= new_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ch[i] = ptr[i];
+			ch[i] = *((char *)ptr + 1);
 	}
 	if (old_size < new_size)
 	{
 		for (i = 0; i < new_size; i++)
-			ch[i] = ptr[i];
+			ch[i] = *((char *)ptr + 1);
 	}
 	return (ch);
 
