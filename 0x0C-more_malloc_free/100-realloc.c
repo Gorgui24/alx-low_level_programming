8d8eb1cbbd5a5f6524b889aed1a2ada56fd65f36
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-09 09:06:32 -0700

    Alx Low Level Programming

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include<string.h>
+#include<stdlib.h>
+/**
+ * _realloc - return adress
+ * @old_size: int parameter
+ * @new_size: int parameter
+ * @ptr: pointer parameter
+ * Return: pointer address
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ch;
+	int i;
+
+	if (old_size == 0 && new_size == 0)
+		return (NULL)
+	ch = malloc(old_size);
+	if (ch == NULL || old_size < new_size)
+		ch = malloc(new_size);
+	if (old_size >= new_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ch[i] = ptr[i];
+	}
+	if (old_size < new_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ch[i] = ptr[i];
+	}
+	return (ch);
+
+}
