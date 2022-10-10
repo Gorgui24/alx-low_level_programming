7def5d6e47fdf429e3e1b2f8a9baf3705796e33c
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-10 13:35:15 -0700

    Alx Low Level Programming

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
-struct dog 
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - define struct
+ * @name: char pointer
+ * @age: float type
+ * @owner: char pointer
+ */
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
