ee8f7e141d7b4b1feebb2a9db9e64149aa8cb68f
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-10 14:13:41 -0700

    Alx Low Level Programming

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,15 +2,16 @@
 /**
  * malloc_checked - return adress
  * @d: struct parameter
- * name: char parameter
- * age: float parameter
- * owner: char parameter
+ * @name: char parameter
+ * @age: float parameter
+ * @owner: char parameter
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return (NULL);
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
