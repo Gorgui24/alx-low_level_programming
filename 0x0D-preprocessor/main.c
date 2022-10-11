7e980a59d0c86af53f7b4dc3c76ffdaaae515de5
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-10 17:07:31 -0700

    Alx Low Level Programming

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "0-object_like_macro.h"
+/**
+ * main - print a to z
+ *
+ * Retun: 0
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
