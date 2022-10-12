d85db314a898947faa7f826791d5b5afd49c63e1
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-12 15:43:50 -0700

    Alx Low Level Programming

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stddef.h>
+#include <stdio.h>
 /**
  * array_iterator - print array
  * @array: array parameter
@@ -7,12 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
-
+	unsigned int i;
 	if (!array || !action)
 		return;
 	size = sizeof(array);
 	for (i = 0; i < size; i++)
-		action(array[i];
+		action(array[i]);
 
 }
