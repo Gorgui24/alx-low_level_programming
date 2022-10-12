a6b7c2b49d8a1dca0691077b30e330f6acacdaa5
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-12 16:53:04 -0700

    Alx Low Level Programming

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,10 +2,11 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
- * array_iterator - print array
+ * int_index - print array
  * @array: array parameter
  * @size: int parameter
  * @cmp: pointer parameter
+ * Return: int
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -16,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
-					return (i);
+				return (i);
 		}
 	}
 	return (-1);
