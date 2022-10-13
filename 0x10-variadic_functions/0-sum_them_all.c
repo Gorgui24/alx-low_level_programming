6217c1cbb5d7fa9b43202c0ec8d2974f563bdfd3
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-13 14:12:43 -0700

    Alx Low Level Programming

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - return adress
+ * @n: char parameter
+ * Return: retunr 1
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return 0;
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += sum;
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * main - print a to z
+ *
+ * Retun: 0
+ */
+int main(void)
+{
+	return (0);
+}
