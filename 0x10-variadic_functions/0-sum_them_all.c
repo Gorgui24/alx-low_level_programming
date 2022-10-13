23b02f60b36d698c0fdfe4536a9d11a635eec057
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-13 14:52:35 -0700

    Alx Low Level Programming

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,8 +10,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	int i;
 	int sum = 0;
+	if (n == 0)
+		return (sum);
 
-	if (n != 0)
+	if (n >= 0)
 	{
 		va_start(ap, n);
 		for (i = n; i >= 0; i = va_arg(ap, int))
