dca88aa667a12cb87889360d984e208d0e52fb19
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-13 15:30:09 -0700

    Alx Low Level Programming

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,12 +1,30 @@
-#include "function_pointers.h"
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h> 
 /**
- * print_name - return adress
- * @name: char parameter
- * @f: pointer paraameter
+ * print_numbers - return adress
+ * @n: char parameter
+ * @separator: pointer parameter
  */
-void print_name(char *name, void (*f)(char *))
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	if (!name || !f)
+	va_list ap;
+	unsigned int i;
+	char ch;
+
+	if (separator == NULL)
 		return;
-	f(name);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		ch = separator[i];
+		if (ch >= '0' && ch <= '9')
+		{
+			if (i != n - 1)
+				printf("%d, ", va_arg(ap, int));
+			else
+				printf("%d\n", va_arg(ap, int));
+		va_end(ap);
+		}
+	}
 }
