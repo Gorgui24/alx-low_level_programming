6df0791fc81618c09e11fdeb535c837d88e503b2
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-20 17:35:54 -0700

    Alx Low Level Programming

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 /**
- * main - print before main
- * Return: 0
+ * printBeforeMain - print before main
  */
 void printBeforeMain(void) __attribute__ ((constructor));
+
 void printBeforeMain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature");
-	printf(", challenges one to a race.)\n");
-	return (0);
 }
