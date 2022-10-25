04c209fd61d9f63d384ba465128f07772491ce7f
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-25 16:12:58 -0700

    Alx Low Level Programming

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * add_nodeint - print
+ * @head: double pointer parameter
+ * @n: int parameter
+ * Return: node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ajout;
+
+	ajout = malloc(sizeof(listint_t));
+	if (ajout == NULL)
+		return (NULL);
+	ajout->n = n;
+	ajout->next = *head;
+	return (ajout);
+}
