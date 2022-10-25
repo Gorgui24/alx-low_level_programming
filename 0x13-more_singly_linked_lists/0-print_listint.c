42a5161d241d2a7cca6024d882f4fc67e1927fcf
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-10-25 15:51:28 -0700

    Alx Low Level Programming

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - print
+ * @h: pointer parameter
+ * Return: node
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
