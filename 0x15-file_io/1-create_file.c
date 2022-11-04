0ebb483bfb04e7191928b96955233b91e92ac103
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-11-03 17:08:21 -0700

    Alx Low Level Programming

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,7 +17,6 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	len = 0;
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
 	w = write(o, text_content, len);
 	if (o == -1 || w == -1)
