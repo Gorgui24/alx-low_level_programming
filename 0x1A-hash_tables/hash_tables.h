4f71fb5f744dea8ba647752071cc183027abeab4
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-01-06 12:42:55 -0800

    Alx Low Level Programming

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,8 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -10,7 +15,7 @@ typedef struct hash_node_s
 {
 	char *key;
 	char *value;
-	truct hash_node_s *next;
+	struct hash_node_s *next;
 } hash_node_t;
 /**
  * * struct hash_table_s - Hash table data structure
@@ -25,3 +30,6 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+#endif
