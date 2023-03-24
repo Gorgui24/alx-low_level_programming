ac4b51a7b4857bbefae766f453b16371ef727d1a
Gorgui24 <fayesouleymanegorgui@example.com>
2023-03-24 16:48:46 -0700

    File Io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,28 +1,54 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_file - i/O
- * @filename: pointer parameter
- * @text_content: int parameter
- * Return: return int
+ * main - Copies content of file to another
+ *
+ * @argc: int
+ *
+ * @argv: double pointer
+ *
+ * Return: Copy of file
  */
-int create_file(const char *filename, char *text_content)
-{
-	int o, w, len = 0;
 
-	if (filename == NULL)
-		return (-1);
-	if (text_content != NULL)
+int main(int argc, char **argv)
+{
+	int fd, fd2, filecheck;
+	char buffer[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
-	w = write(o, text_content, len);
-	if (o == -1 || w == -1)
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((filecheck = read(fd, buffer, 1024)) > 0)
 	{
-		return (-1);
+		if (filecheck == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		filecheck = write(fd2, buffer, filecheck);
+		if (filecheck == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-	close(o);
-	return (1);
+	if (filecheck == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+
+	return (0);
 }
